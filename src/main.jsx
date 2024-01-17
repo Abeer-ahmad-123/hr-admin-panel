a79698ede6ca2635100255887e26c08702b20110
mac <mac@MacBook-Pro.local>
2024-01-17 17:09:58 +0500

    redux-toolkit complete

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 import App from './app';
-import store from './redux-toolkit/store';
+import store from './Redux-toolkit/store';
+// import store from './redux-toolkit/store';
 
 // ----------------------------------------------------------------------
 
--- src/redux-toolkit/actions/userActions.js
+++ src/redux-toolkit/actions/userActions.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 //-----------------------------------------------------------------------
-
 export const allUsers = createAsyncThunk('user/allUsers', async (_, { rejectWithValue }) => {
+  const url = import.meta.env.VITE_REACT_APP_BASE_URL;
+
   try {
-    const response = await axios.get('https://api.enxsis.com/api/v1/users');
+    const response = await axios.get(`${url}/users`);
 
     console.log('data', response.data);
     return response.data;
